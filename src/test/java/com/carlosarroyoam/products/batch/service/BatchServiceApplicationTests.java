c22d60da23dd2ab87295aafc7860fc53eb18f6cc
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2026-04-26 21:15:27 -0600

    Renombra test package

--- src/test/java/com/carlosarroyoam/products/batch/service/BatchServiceApplicationTests.java
+++ src/test/java/com/carlosarroyoam/products/batch/service/BatchServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.carlosarroyoam.batch_service;
+package com.carlosarroyoam.products.batch.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
